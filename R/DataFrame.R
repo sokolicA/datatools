85385586aa4d224d1e140ee4b0b8e6f81b348f24
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-03-18 17:46:53 +0100

    added base initialization to dataframe

--- R/DataFrame.R
+++ R/DataFrame.R
@@ -1,10 +1,16 @@
 DataFrame <- R6::R6Class(
     "DataFrame",
     public = list(
+        initialize = function(tbl, primary_key = NULL, id = NULL) {
+            data.table::setDT(tbl, key = primary_key)
+            private$.tbl <- tbl
+            private$.id <- id
+        }
 
     ),
 
     private = list(
-
+        .tbl = NULL,
+        .id = NA_character_
     )
 )
