acfb594644b7ebc3e999d279ad47dc89a5595e6d
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-03-18 18:40:21 +0100

    added count method

--- R/DataFrame.R
+++ R/DataFrame.R
@@ -21,6 +21,10 @@ DataFrame <- R6::R6Class(
 
         is_key_unique = function() {
             uniqueN(private$.tbl, by = get_key(private$.tbl)) == nrow(private$.tbl)
+        },
+
+        count = function(by="") {
+            private$.tbl[, .N, keyby = by]
         }
 
     ),
