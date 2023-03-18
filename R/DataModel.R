f2c3dcbcd67097ea9346dda1af6a8f72e60b5de4
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-03-18 17:37:06 +0100

    added base initialization structure

--- R/DataModel.R
+++ R/DataModel.R
@@ -1,10 +1,23 @@
 DataModel <- R6::R6Class(
     "DataModel",
     public = list(
+        initialize = function(data = NULL, relationship = NULL) {
+            if (!is.null(data)) self$add_data(data)
+            if (!is.null(relationship)) self$add_relationship(relationship)
+        },
+
+        add_data = function() {
+
+        },
+
+        add_relationship = function() {
+
+        }
 
     ),
 
     private = list(
-
+        .data = NULL,
+        .relationships = NULL
     )
 )
