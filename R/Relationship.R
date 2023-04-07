ec7605e35b931bf13b438b879fc61d603e8fa981
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-04-07 08:11:14 +0200

    finished basic documentation of relationship class

--- R/Relationship.R
+++ R/Relationship.R
@@ -7,26 +7,45 @@
 Relationship <- R6::R6Class(
     "Relationship",
     public = list(
+        #' @description Relationship constructor
+        #'
+        #' @param left Optional object of class `data.table` (or `data.frame`, see Details) that is treated as the left table.
+        #' @param right Optional object of class `data.table` (or `data.frame`, see Details) that is treated as the right table.
         initialize = function(left=NULL, right=NULL) {
             if(!is.null(left)) self$left <- left;
             if(!is.null(right)) self$right <- right;
         },
 
+        #' @description Get the relationship connection.
+        #'
+        #' @return The connection.
+        #'
         get_on = function() {
             private$.on
         },
 
+        #' @description Specify the columns the relationship between left and right is based on.
+        #'
+        #' @param ... Which columns in the left table relate to which columns of the right table.
+        #'
+        #' @return Invisibly returns itself.
+        #'
         on = function(...) {
             private$.on <- substitute(list(...))
             return(invisible(self))
         },
 
+        #' @description Check if the relationship is not fully specified.
+        #'
+        #' @return `TRUE` if `left`, `right` and `on` are specified and `FALSE` otherwise.
+        #'
         is_not_fully_specified = function() {
             return(any(is.null(private$.left), is.null(private$.right), is.null(private$.on)))
         }
     ),
 
     active = list(
+        #' @field left The left table.
         left = function(x) {
             if (!missing(x)) {
                 if (!is.data.table(x)) x <- as.data.table(x)
@@ -36,6 +55,7 @@ Relationship <- R6::R6Class(
             }
         },
 
+        #' @field right The right table.
         right = function(x) {
             if (!missing(x)) {
                 if (!is.data.table(x)) x <- as.data.table(x)
@@ -50,6 +70,5 @@ Relationship <- R6::R6Class(
         .left = NULL,
         .right = NULL,
         .on = NULL
-
     )
 )
