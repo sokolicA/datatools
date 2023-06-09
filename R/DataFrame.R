8290f9d5a6a257058d50ce8ed4b97a54c5d13064
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-06-09 08:41:03 +0200

    changed alist to list

--- R/DataFrame.R
+++ R/DataFrame.R
@@ -496,7 +496,7 @@ DataFrame <- R6::R6Class(
         #' @examples
         #' TODO
         insert = function(...) {#browser()
-            e <- substitute(alist(...))
+            e <- substitute(list(...))
             if (is.null(names(e)) || any(names(e)[-1L]=="")) stop("Must pass named columns!")
             if (any(names(e) %in% names(private$tbl))) stop("Some columns already exist!")
             e[[1L]] <- quote(`:=`)
