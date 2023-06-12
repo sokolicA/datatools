7f7329b63432adb651f14fa59b5359991867fc61
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-06-12 10:33:43 +0200

    added DTCall class

---
+++ R/DTCall.R
@@ -0,0 +1,72 @@
+#' @title data.table call
+#'
+#' @description
+#'
+#' @import R6
+DTCall <- R6::R6Class(
+    "DTExpr",
+    cloneable = FALSE,
+
+    public = list(
+
+        #' @description Constructor.
+        #'
+        #' @param x Name of the data.table.
+        #'
+        #' @return Invisibly returns itself.
+        #'
+        initialize = function(x=private$tbl) {
+            private$expr <- as.call(list(quote(`[`), x=substitute(x)))
+            invisible(self)
+        },
+
+        #' @description Default print method.
+        #'
+        #' @return Invisibly returns itself.
+        #'
+        print = function() {
+            print(private$expr)
+            invisible(self)
+        },
+
+        #' @description Sets (or unsets) arguments in the `data.table` call.
+        #'
+        #' @param ... Named language (or symbol) arguments that are used in the `data.table` call.
+        #'
+        #' @return Invisibly returns itself.
+        #'
+        set = function(...) {
+            args <- list(...)
+            #private$expr[names(args)] <- unlist(args) # looks nicer but does not work for removing elements.
+            for (arg in names(args)) {
+                if (is.null(private$expr[[arg]]) && is.null(args[[arg]])) next;
+                private$expr[[arg]] <- args[[arg]]
+            }
+            invisible(self)
+        },
+
+        #' @description Get the call object.
+        #'
+        #' @param ... Named language (or symbol) arguments that are used in the `data.table` call.
+        #'
+        #' @return Invisibly returns itself.
+        #'
+        call = function() {
+            private$expr
+        }
+    ),
+
+    private = list(
+
+        expr = NULL
+
+    )
+)
+
+
+# For non-quoted arguments
+# direct_set = function(...) {browser()
+#     args <- substitute(list(...()))
+#     private$expr[names(args)] <- args
+#     invisible(self)
+# }
