5c2ba524cf2545235c6e45ce15da56df58437a74
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-06-16 08:34:00 +0200

    added new tests for by/keyby exclusion

--- tests/testthat/test-DTCall.R
+++ tests/testthat/test-DTCall.R
@@ -11,15 +11,15 @@ test_that("setting arguments in DTCall works",
 test_that("renaming table works through DTCall$set",
           {
               e <- DTCall$new(tbl)
-              e$set(x=quote(y))
-              expect_equal(e$call(), quote(`[`(x= y)))
+              e$set(x=quote(y), i = quote(a==2))
+              expect_equal(e$call(), quote(`[`(x= y, i= a==2)))
           })
 
 test_that("removing and overriding arguments in DTCall works",
           {
               e <- DTCall$new(tbl)
               e$set(i = NULL, j = NULL)
-              expect_equal(e$call(), quote(`[`(x= tbl)))
+              expect_equal(e$call(), quote(tbl))
 
               e$set(i = quote(a==3), j = quote(.SD))
               e$set(i = NULL, j = quote(`:=` (y = x)))
@@ -47,4 +47,25 @@ test_that("call returns only x (the name of the table) if neither i nor j are sp
               e$set(by=quote(a))
 
               expect_equal(e$call(), quote(tbl))
+          })
+
+test_that("call subset works",
+          {
+              e <- DTCall$new(tbl)
+              e$set(i = quote(a==3), j = quote(.SD), by=quote(a))
+
+              expect_equal(e$call(), quote(`[`(x=tbl, i=a==3, j=.SD, by=a)))
+              expect_equal(e$call(subset="i"), quote(`[`(x=tbl, i=a==3)))
+          })
+
+
+test_that("by and keyby are mutually exclusive with warning upon override",
+          {
+              e <- DTCall$new(tbl)
+              e$set(i=quote(a), by=quote(a))
+
+              expect_warning(e$set(keyby=quote(a)))
+              expect_equal(e$call(), quote(`[`(x=tbl, i=a, keyby=a)))
+              expect_warning(e$set(by=quote(a)))
+              expect_equal(e$call(), quote(`[`(x=tbl, i=a, by=a)))
           })
