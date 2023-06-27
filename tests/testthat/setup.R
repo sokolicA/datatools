20895937a1c32dcda428cd19ba6810edd8d6a435
Andrej Sokolič <andrejsokolicc@gmail.com>
2023-06-27 09:24:41 +0200

    updated to use new extend_body

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -8,17 +8,12 @@ test_private <- function(method, class, debug=FALSE) {
 }
 
 with_debugger <- function(f) {
-    b <- body(f)
-    if (is.null(b)) stop("Function has no body!")
-
-    if (length(b) == 1 || b[[1]] != quote(`{`)) b <- call("{", b)
-    nb <- as.call(append(as.list(b), quote(browser()), after=1))
-
-    body(f) <- nb
-    f
+    extend_body(f, quote(browser()), 1L)
 }
 #
 # f1 <- mean
 # f2 <- is.integer # error
 # f3 <- function(x) x
 # f4 <- function(x) {x}
+
+
